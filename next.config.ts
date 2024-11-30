ed6b16b62e991fa4752f401fcfdfc94ad41516d4
Hyunwoo <nhw3990@kau.kr>
2024-12-01 00:14:50 +0900

    feat: Next.js Image 컴포넌트 S3 도메인 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: true
+  },
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'traffic-incident-s3-bucket.s3.ap-northeast-2.amazonaws.com',
+      port: '',
+      pathname: '/recordings/frames/**'
+    }]
   }
 };
 
